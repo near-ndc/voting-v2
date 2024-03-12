dbfe7ace4f8e01ef4e9389109648eb4636ee7341
akorchyn <artur.yurii.korchynskyi@gmail.com>
2024-03-12 11:48:05 +0200

    add single transaction hash per active month

--- snapshotter/sql/user_activity.sql
+++ snapshotter/sql/user_activity.sql
@@ -11,15 +11,36 @@ CREATE TABLE ActiveMonthsPerSigner AS WITH DistinctStakedSigners AS (
         and t.status = 'SUCCESS'
         and ra.args similar to '%"method_name": "(deposit_and_stake|stake)"%'
         and t.block_height < 108194271
+),
+TransactionsWithRowNumber AS (
+    SELECT
+        t.signer_account_id,
+        t.transaction_hash,
+        TO_CHAR(t.block_date, 'YYYYMM') as formatted_date,
+        ROW_NUMBER() OVER (
+            PARTITION BY t.signer_account_id,
+            TO_CHAR(t.block_date, 'YYYYMM')
+            ORDER BY
+                t.block_date
+        ) as rn
+    FROM
+        transactions t
+        JOIN DistinctStakedSigners ds ON ds.signer_account_id = t.signer_account_id
 )
 SELECT
     ds.signer_account_id,
-    COUNT(
-        DISTINCT TO_CHAR(t.block_date, 'YYYYMM')
-    ) as active_months,
-    COUNT(t.transaction_hash) as transactions
+    COUNT(DISTINCT tr.formatted_date) as active_months,
+    COUNT(tr.transaction_hash) as transactions,
+    /* One transaction hash per each active month */
+    STRING_AGG(
+        CASE
+            WHEN tr.rn = 1 THEN tr.transaction_hash
+            ELSE NULL
+        END,
+        ','
+    ) as example_transaction_hashes
 FROM
     DistinctStakedSigners ds
-    JOIN transactions t ON ds.signer_account_id = t.signer_account_id
+    JOIN TransactionsWithRowNumber tr ON ds.signer_account_id = tr.signer_account_id
 GROUP BY
     ds.signer_account_id;
